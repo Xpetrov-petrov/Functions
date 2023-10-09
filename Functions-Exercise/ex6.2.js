23c15d1a422e633ef9fd3b854618ddbdcc29a0f1
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-10-09 15:18:51 +0300

    ++

---
+++ Functions-Exercise/ex6.2.js
@@ -0,0 +1,66 @@
+function passwordValidator(pass) {
+
+    let ischeckValidPassLength = checkValidPassLength(pass);
+    let ischeckOnlyLettersDigits = checkOnlyLettersDigits(pass);
+    let ischeck2Digits = check2Digits(pass);
+
+    if (ischeck2Digits && ischeckOnlyLettersDigits && ischeckValidPassLength) {
+        console.log("Password is valid");
+    }
+
+
+    function checkValidPassLength(pass) {
+        if (pass.length >= 6 && pass.length <= 10) {
+            return true;
+        } else {
+            console.log("Password must be between 6 and 10 characters");
+            return false;
+        }
+    }
+
+    function checkOnlyLettersDigits(pass) {
+        for (let curChar of pass) {
+            let code = curChar.charCodeAt(0);
+
+            if (
+                !((code >= 48 && code <= 57) ||
+                    (code >= 65 && code <= 90) ||
+                    (code >= 97 && code <= 122))
+            ) {
+                console.log("Password must consist only of letters and digits");
+                return false;
+            }
+
+
+        }
+        return false;
+        
+    }
+
+
+    function check2Digits(pass) {
+    let digitsCounter = 0;
+
+    for (let curChar of pass) {
+        let code = curChar.charCodeAt(0);
+
+        if (code >= 48 && code <= 57) {
+            digitsCounter++;
+        }
+    }
+
+    if (digitsCounter < 2) {
+        console.log("Password must have at least 2 digits");
+        return false;
+    } else {
+        return true;
+    }
+}
+}
+
+
+
+
+//passwordValidator('logIn');
+//passwordValidator('MyPass123');
+passwordValidator('Pa$s$s');
---
+++ Functions-Exercise/ex7.js
@@ -0,0 +1,9 @@
+function nxnMatrix(num){
+
+    for(let i = 1; i <= num; i++){
+    let b = `${num} `.repeat(num);
+    console.log(b);
+    }
+    
+}
+nxnMatrix(7);
